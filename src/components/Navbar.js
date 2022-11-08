a70917f04bf7b6b3ebeb6c8266d377c0a84917d7
Arya Nandakumar <74709811+Arya-Nandakumar@users.noreply.github.com>
2022-11-08 10:59:28 +0530

    Real Time Chat App Deployment

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { signOut } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
+import { AuthContext } from "../context/auth";
+import { useHistory } from "react-router-dom";
+
+const Navbar = () => {
+  const history = useHistory();
+  const { user } = useContext(AuthContext);
+
+  const handleSignout = async () => {
+    await updateDoc(doc(db, "users", auth.currentUser.uid), {
+      isOnline: false,
+    });
+    await signOut(auth);
+    history.replace("/login");
+  };
+  return (
+    <nav>
+      <h3>
+        <img src="https://www.pngarts.com/files/1/Message-Transparent-Background-PNG.png"></img>
+        <Link to="/">ChatVia</Link>
+      </h3>
+      <div>
+        {user ? (
+          <>
+            <button className="btn" onClick={handleSignout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
